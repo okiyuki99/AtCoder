f4a531d69b3c7f7261771868d9f450370edef644
gingi99 <bigtree3101@gmail.com>
2019-09-16 14:35:49 +0900

    add abc126 d

---
+++ solutions/abc126/d.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+N = int(input())
+S = [[]for i in range(N)]
+for i in range(N-1):
+    u,v,w = map(int,input().split())
+    S[u-1].append([v-1,w])
+    S[v-1].append([u-1,w])
+#print(S)
+
+Node = [None]*N
+Node[0] = True
+queue = deque([0])
+while queue != deque([]):
+    #print(queue)
+    #print(Node)
+    p = queue.popleft()
+    for u,w in S[p]:
+        if Node[u] == None:
+            queue.append(u)
+            Node[u] = (Node[p])^(w % 2 == 1)
+for i in range(N):
+    print([0,1][Node[i]])
